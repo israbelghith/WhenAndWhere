59669c778c28a66aa34507347f4a6482c478d0df
israbelghith <97345077+israbelghith@users.noreply.github.com>
2025-07-07 15:08:48 +0200

    Add getallactivities

--- src/main/java/com/waw/whenandwhere/controllers/ActivityController.java
+++ src/main/java/com/waw/whenandwhere/controllers/ActivityController.java
@@ -1,5 +1,25 @@
 package com.waw.whenandwhere.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.waw.whenandwhere.entities.Activity;
+import com.waw.whenandwhere.services.ActivityService;
+
+@RestController
+@RequestMapping("/api/activities")
 public class ActivityController {
 
+	@Autowired
+	private ActivityService activityService;
+
+    
+    @GetMapping
+    public List<Activity> getAllActivities() {
+        return activityService.findAllActivities();
+    }
 }
